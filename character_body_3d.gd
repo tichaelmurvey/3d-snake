28ff528fa06d4eecb88bf1af70756294e50a22f3
Tawfiq H <tawfiq.hamid@gmail.com>
2026-03-16 17:31:41 +0000

    Added WASD, and collision detection and singleton script game.gd

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 15.0
 const STEERING_POWER = 2.0
 
 
@@ -10,7 +10,20 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if input_dir:
-		rotation -= Vector3(input_dir.y, input_dir.x, 0)*STEERING_POWER*delta
-	velocity = global_transform.basis.z*SPEED
+		rotation -= Vector3(input_dir.y, input_dir.x, 0) * STEERING_POWER * delta
+	velocity = global_transform.basis.z * SPEED
 	
 	move_and_slide()
+	var collision: KinematicCollision3D = get_last_slide_collision()
+	if collision:
+		handle_collision(collision)
+
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Collision Detection
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# const HIT_LAYER_FILTER := (1 << 1) | (1 << 2) # layers 2 and 3
+func handle_collision(collision: KinematicCollision3D) -> void:
+	var collider = collision.get_collider()
+	print("Collision detected with: ", collision.get_collider().name)
+	
